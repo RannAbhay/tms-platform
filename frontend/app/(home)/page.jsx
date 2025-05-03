83c3ec4944abdfa6489af3c613851e6d4a3854bd
RannAbhay <vedantlegendx@gmail.com>
2025-05-03 22:37:46 +0530

    Made the login component

--- frontend/app/(home)/page.jsx
+++ frontend/app/(home)/page.jsx
@@ -11,15 +11,60 @@ const HomeLayout = () => {
             <TabsTrigger value="signup">Signup</TabsTrigger>
           </TabsList>
           <TabsContent value="login" className="text-center">
-            <h1>Login</h1>
-            <Input
-              name="email"
-              className="min-w-[270px] sm:w-[450px]"
-              placeholder="Enter your Email..."
-            />
+            <h1 className="mb-4">Login</h1>
+            <div className="mb-5">
+              <Input
+                name="email"
+                className="min-w-[270px] sm:w-[450px]"
+                placeholder="Enter your Email..."
+              />
+              <p className="text-blue-500 underline text-sm text-right w-full mt-1 cursor-pointer hover:opacity-80 transition-opacity">
+                Forgot Email
+              </p>
+            </div>
+            <div>
+              <Input
+                name="password"
+                className="min-w-[270px] sm:w-[450px]"
+                placeholder="Enter your Password"
+              />
+              <p className="text-blue-500 underline text-sm text-right w-full mt-1 pb-4 cursor-pointer hover:opacity-80 transition-opacity">
+                Forgot Password
+              </p>
+            </div>
           </TabsContent>
           <TabsContent value="signup" className="text-center">
-            <h1>Signup</h1>
+            <h1 className="mb-4">Signup</h1>
+            <div className="mb-5">
+              <Input
+                name="username"
+                className="min-w-[270px] sm:w-[450px]"
+                placeholder="Enter your UserName..."
+              />
+              <p className="text-blue-500 underline text-sm text-right w-full mt-1 cursor-pointer hover:opacity-80 transition-opacity">
+                Forgot UserName
+              </p>
+            </div>
+            <div className="mb-5">
+              <Input
+                name="email"
+                className="min-w-[270px] sm:w-[450px]"
+                placeholder="Enter your Email..."
+              />
+              <p className="text-blue-500 underline text-sm text-right w-full mt-1 cursor-pointer hover:opacity-80 transition-opacity">
+                Forgot Email
+              </p>
+            </div>
+            <div>
+              <Input
+                name="password"
+                className="min-w-[270px] sm:w-[450px]"
+                placeholder="Enter your Password"
+              />
+              <p className="text-blue-500 underline text-sm text-right w-full mt-1 pb-4 cursor-pointer hover:opacity-80 transition-opacity">
+                Forgot Password
+              </p>
+            </div>
           </TabsContent>
         </Tabs>
       </div>
